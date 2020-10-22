bd35c09bd475b317fb2c3667e02e32d47394a35f
icenine79 <vitor.m.c.n.ferreira@gmail.com>
2020-10-22 14:48:30 +0100

    ad

--- src/app/admin/components/admin-detail/admin-detail.component.ts
+++ src/app/admin/components/admin-detail/admin-detail.component.ts
@@ -1,15 +1,20 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../shared/services/user.service';
+import { User } from '../../../global-features/models/User';
 
 @Component({
   selector: 'app-admin-detail',
   templateUrl: './admin-detail.component.html',
-  styleUrls: ['./admin-detail.component.scss']
+  styleUrls: ['./admin-detail.component.scss'],
 })
 export class AdminDetailComponent implements OnInit {
-  constructor (private route: ActivatedRoute) {}
+  user: any;
+  constructor(private route: ActivatedRoute) {}
   ngOnInit() {
-     this.route.data.subscribe(data => console.log('Data :', data));
+    this.route.data.subscribe((data:any) => {
+      this.user=Array.of(data);
+      console.log(this.user)
+    });
   }
 }
