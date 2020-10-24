8c060d5856ce40f48bf4b73a3a44664a2998c18b
icenine79 <vitor.m.c.n.ferreira@gmail.com>
2020-10-24 01:43:57 +0100

    new login

--- src/app/app-components/login/login.component.ts
+++ src/app/app-components/login/login.component.ts
@@ -3,11 +3,13 @@ import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Form } from '../../global-features/dynamic-forms/Form';
 import { AuthService } from '../../shared/services/auth.service';
+import { Fader } from '../../shared/animations/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  animations: [Fader.animations]
 })
 export class LoginComponent extends Form implements OnInit {
   constructor(private auth: AuthService, private router:Router) {
